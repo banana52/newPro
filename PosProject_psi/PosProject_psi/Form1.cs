adad43eae050e2233571ea987a01cbc19a526531
LWS <abc@abc.com>
2018-01-03 17:43:59 +0900

    main

---
+++ PosProject_psi/PosProject_psi/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PosProject_psi
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            itemGrid.ColumnCount = 5;
+            itemGrid.Columns[0].Name = "NO";
+            itemGrid.Columns[1].AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            itemGrid.Columns[1].Name = "상 품 명";
+            itemGrid.Columns[2].Name = "단 가";
+            itemGrid.Columns[3].Name = "수 량";
+            itemGrid.Columns[4].Name = "할 인";
+            lblDate.Text = DateTime.Now.Month + "월 " + DateTime.Now.Day + "일 " + DateTime.Now.ToLongTimeString();
+        }
+    }
+}
